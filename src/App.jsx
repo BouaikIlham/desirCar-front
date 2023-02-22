c1573fc392d6ed8fa2a708fca953037ea2a1f40b
ilham bouaik <ibouaik@gmail.com>
2023-02-22 11:16:37 +0000

    install tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ function App() {
 
   return (
     <div className="App">
-      
+        <h1 className="text-3xl font-bold underline decoration-sky-500">
+          Hello world!
+        </h1>
     </div>
   )
 }
